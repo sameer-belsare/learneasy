7643f9317d3a37a1a8198a4aacc0ee751a5c0791
sameer-belsare <sameer.belsare@gmail.com>
2019-06-03 13:37:35 +0530

    renamed LearnEasyApp to main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:learneasy/LearnEasyApp.dart';
+import 'package:learneasy/main.dart';
 
 void main() {
   testWidgets('test', (WidgetTester tester) async {
